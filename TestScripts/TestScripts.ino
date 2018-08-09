f10fa77eb1713fbeb17479724588c91816240e02
G0057Y <guscres@gmail.com>
2018-08-09 14:09:09 -0300

    Prueba de matrices en el Test

--- TestScripts/TestScripts.ino
+++ TestScripts/TestScripts.ino
@@ -5,9 +5,11 @@
 const int DH[3][3]={{0, 0, 0},{150, 140, 0},{0, 0, 0}};
 //mtx_type DH2[3][3]={{0, 150, 0}, {0, 140, 0}, {0, 0, 0}};
 float angulos[3];
-const int Matriz[4][4]={{1, 0, 0, 1}, {0, 1, 0, 0}, {0, 0, 1, 0},{0, 0, 0, 1}};
-//mtx_type A[4][4]={{1, 0, 0, 0}, {0, 1, 0, 0}, {0, 0, 1, 0}, {0, 0, 0, 1}};
+const float Identidad[4][4]={{1, 0, 0, 0}, {0, 1, 0, 0}, {0, 0, 1, 0},{0, 0, 0, 1}};
+//mtx_type Identidad[4][4]={{1, 0, 0, 0}, {0, 1, 0, 0}, {0, 0, 1, 0}, {0, 0, 0, 1}};
 float Angulos[3]={30*M_PI/180, 20*M_PI/180, -10*M_PI/180};
+float Ri[4][4];
+float R03[4][4];
 
 // PAra usar matrices, los subindices empiezan en 0, es decir, 
 // el ultimo valor de una matriz de 3x3 es el M[2][2]
@@ -25,6 +27,11 @@ void loop(){
 //  Serial.println(Matriz[3][0], DEC);
 //  Serial.println(DH[1][0], DEC);
 //  Serial.println(Matriz[3][2], DEC);
+  R03 = Identidad;
+  for (int i=0; i=2; i++){
+    Ri={{cos(Angulos[i]), -sin(Angulos[i]), 0, DH[i][2]*cos(Angulos[i])}, {sin(Angulos[i]), cos(Angulos[i]), 0, DH[i][2]*sin(Angulos[i])}, {0, 0, 1,0}, {0, 0, 0, 1}};
+    R03= {{R03[1][1]*Ri[1][1]+R03[1][2]*Ri[2][1], R03[1][1]*Ri[1][2]+R03[1][2]*Ri[2][2], 0,R03[1][1]*Ri[1][4]+R03[1][2]*Ri[2][4]+R03[1][4]}, {R03[2][1]*Ri[1][1]+R03[2][2]*Ri[2][1], R03[2][1]*Ri[1][2]+R03[2][2]*Ri[2][2], 0,R03[2][1]*Ri[1][4]+R03[2][2]*Ri[2][4]+R03[2][4]}, {0, 0, 1,0}, {0, 0, 0, 1}};
+  };
   Serial.println(Angulos[1], DEC);
   //Serial.println(Matriz, DEC);
   //Matrix.Print((mtx_type*)DH2, 3, 3, "DH");
@@ -42,4 +49,10 @@ void loop(){
 //  return Radianes;
 //}
   
-
+  
+//  R(i-1,i)={{cos(Angulos[i]), -sin(Angulos[i]), 0, DH[i][2]*cos(Angulos[i])}, {sin(Angulos[i]), cos(Angulos[i]), 0, DH[i][2]*sin(Angulos[i])}, {0, 0, 1,0}, {0, 0, 0, 1}};
+//R03 = Identidad,
+//for (int i=0; i=2, i++){
+  //Ri={{cos(Angulos[i]), -sin(Angulos[i]), 0, DH[i][2]*cos(Angulos[i])}, {sin(Angulos[i]), cos(Angulos[i]), 0, DH[i][2]*sin(Angulos[i])}, {0, 0, 1,0}, {0, 0, 0, 1}};
+  //R03= {{R03[1][1]*Ri[1][1]+R03[1][2]*Ri[2][1], R03[1][1]*Ri[1][2]+R03[1][2]*Ri[2][2], 0,R03[1][1]*Ri[1][4]+R03[1][2]*Ri[2][4]+R03[1][4]}, {{R03[2][1]*Ri[1][1]+R03[2][2]*Ri[2][1], R03[2][1]*Ri[1][2]+R03[2][2]*Ri[2][2], 0,R03[2][1]*Ri[1][4]+R03[2][2]*Ri[2][4]+R03[2][4]}, {0, 0, 1,0}, {0, 0, 0, 1}};
+  
